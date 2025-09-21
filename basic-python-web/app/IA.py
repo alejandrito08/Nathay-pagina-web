d902d3a3d397faaddbac125024832a35db24541e
alejandrito08 <ariacunan123@gmail.com>
2025-09-21 14:04:24 +0200

    Delete basic-python-web directory

--- basic-python-web/app/IA.py
+++
@@ -1,7 +0,0 @@
-from transformers import pipeline
-
-chatbot = pipeline("text-generation", model="datificate/gpt2-small-spanish")
-
-def respuesta_ia(mensaje_usuario):
-    respuesta = chatbot(mensaje_usuario, max_length=100, do_sample=True)[0]["generated_text"]
-    return respuesta.strip()
